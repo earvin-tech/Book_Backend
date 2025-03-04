3531a416198d67b029686265698a5fdbf347274c
earvin-tech <wsl@Earvin>
2025-03-05 10:34:31 +1100

    Minor changes to schemas and model

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin # type: ignore
 from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
 
 class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,6 @@ from app import ma
 class UserSchema(ma.Schema):
     class Meta:
         fields = ("id", "username", "email", "password_hash", "about_me", "last_seen")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
